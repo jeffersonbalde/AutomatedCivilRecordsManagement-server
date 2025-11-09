b598339e6a02a62a89808c314343bd06816d1bb9
jeffersonbalde <jeffersonbalde8@gmail.com>
2025-11-09 12:14:43 +0800

    modify cors

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3001',
         'https://brimsclient-ihpwt.ondigitalocean.app',
+        'https://automatedcivilrecords-server-nd8c7.ondigitalocean.app',
     ],
 
     'allowed_origins_patterns' => [],
